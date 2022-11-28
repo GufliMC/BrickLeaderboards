5e047ed9c6d199642a865223552ba8cc86e7aaef
jorisguffens <joris.guffens@outlook.com>
2022-11-28 19:08:12 +0000

    Deploying to javadocs from @ GufliMC/BrickLeaderboards@121276bbf82778b13f7e30e39fd22a6e5a27acaa 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.guflimc.brick.leaderboards.api"},{"l":"com.guflimc.brick.leaderboards.api.type"},{"l":"com.guflimc.brick.leaderboards.api.type.podium"}];updateSearchResults();
